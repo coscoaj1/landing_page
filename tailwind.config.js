0496f6e717695d9f4169dbd7604cea766ef37abc
coscoaj1 <coscoanthony1@gmail.com>
2021-11-10 11:03:44 -0500

    build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,22 @@
 module.exports = {
-	purge: [],
-	darkMode: false, // or 'media' or 'class'
-	theme: {
-		extend: {
-			colors: {
-				'regal-black': '#181f1c',
-				button1: '#9c7ca5',
-				button2: '#d9dbf1',
-			},
-		},
-		fontFamily: {
-			body: 'Roboto, system-ui, sans-serif',
-		},
-	},
-	variants: {
-		extend: {},
-	},
-	plugins: [],
+  purge: ["./src/**/*.html", "./src/**/*.js"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "regal-black": "#181f1c",
+        button1: "#9c7ca5",
+        button2: "#d9dbf1",
+      },
+    },
+    fontFamily: {
+      body: "Roboto, system-ui, sans-serif",
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 };
 
 //["1F2324","C1B8A8","575457","C1B8A8","C1B8A8"]
